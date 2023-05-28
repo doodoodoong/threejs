d9808e97226c205a24233b473993867d52ec1d2d
doodoodoong <doo_doong@hanmail.net>
2023-05-28 19:32:59 +0900

    first scene

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 import * as THREE from "three";
 
-const canvas = document.getElementById("#three-canvas");
-
-const renderer = new THREE.WebGLRenderer({ canvas });
+const canvas = document.querySelector("#three-canvas");
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 5;
+camera.position.y = 2;
+camera.position.x = 1;
+scene.add(camera);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: "red",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+renderer.render(scene, camera);
